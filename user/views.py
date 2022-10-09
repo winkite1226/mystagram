344ede96446765eab6312bbbbfbde6efd67c6220
Seongyeon Lee <winkite1226@gmail.com>
2022-10-09 15:22:22 +0900

    complete signup html

--- user/views.py
+++ user/views.py
@@ -4,3 +4,6 @@ from django.contrib import auth  # 사용자 auth 기능
 # Create your views here.
 def sign_in_view(request):
     return render(request, 'user/signin.html')
+
+def sign_up_view(request):
+    return render(request, 'user/signup.html')
